86a0f7fae42b64d2eb37eecccf361f340792f7c5
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-10-29 12:45:08 +0200

    komentarai

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
     <head>
-        <title><!--Komentarai--></title>
+        <title>Komentarai</title>
     </head>
     <body>
-        <h1><!-- Dainų tekstai --></h1>
-        <h2><!-- Paragrafo Daina --></h2>
-        <p><!-- Aš toks vienišas jauans paragrafas...--></p>
-        <p><!-- Mindau svetimo kodo gatves...--></p>
+        <h1>Dainų tekstai</h1>
+        <h2>Paragrafo Daina</h2>
+        <p>Aš toks vienišas jauans paragrafas...</p>
+        <p>Mindau svetimo kodo gatves...</p>
     </body>
 </html>
