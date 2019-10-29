80cd4f508cd0fe5acabafeec8b48ad6e7f6db181
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-10-29 13:24:04 +0200

    rand()date()strtotime()

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <html>
     <head>
-        <title>Komentarai</title>
+        <title>PHP ir lydės <?php print date('Y-m-d', strtotime('+' . rand(1, 10) . 'years')); ?></title>
     </head>
     <body>
-        <h1>Dainų tekstai</h1>
-        <h2>Paragrafo Daina</h2>
-        <?php 
-        //<p>Aš toks vienišas jauans paragrafas...</p>//
-        ?>
-        <!--<p>Mindau svetimo kodo gatves...</p>-->
+        <h1><b>Erikas </b>- Galbūt turėsiu <?php print rand(1, 5); ?> vaikų(us)!</h1>
+        <p>D. Trump'as nebebus prezidentu: <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'years')); ?></p>
     </body>
 </html>
