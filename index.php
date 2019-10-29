a764a14de077978175c7b05bf487e5cb112995ed
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-10-29 11:05:04 +0200

    HTML IR PHP MIXAS

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <html>
 <head>
-    <title>Aš valdau html</title>
+    <title>Aš valdau html <?php print"ir PHP"?> </title>
 </head>
 <body>
-    <h1>Erikas - Html asas</h1>
-    <p>Aš moku kurti statinį turinį.</p>
+    <h1><strong>Erikas</strong> - Html <?php print'ir PHP'?> asas</h1>
+    <p>Kuriu dinaminį turinį.</p>
+    <p>
+        Atidaryta: <?php print date ('Y-m-d H:i:s'); ?>
+    </p>
 </body>
 </html>
