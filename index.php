d1721c2eca3dd6707d5484d464cc8fb6bbe6ef21
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-10-29 12:33:27 +0200

    STRTOTIME IR DATE+-*

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
+<html>
 <head>
     <title> PHP lydės ir <?php print date ('Y-m-d', strtotime('+1 day')); ?></title>
 </head>
 <body>
     <h1><b>Erikas</b>- PHP su manim buvo ir<?php print date(' h', strtotime('-1')).' valandą!';?></h1>
     <p><?php print date('Y ', strtotime('+1 year')) ?>ne už kalnų!</p>
-
 </body>
+</html>
 
