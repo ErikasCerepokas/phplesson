76bdd60aa31b95776a7c8dfbd14cd611e5cf294f
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-10-30 10:52:24 +0200

    IMG IR RANDOM

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <html>
     <head>
-        <style>
-            body {
-                background-color: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255); ?>)
-            }
-            h1 {
-                font-size: <?php print rand(10, 36) ?>
-            }
-            p {
-                color: rgb(<?php print rand(0, 255).','.rand(0, 255).','.rand(0, 255)?>)            
-            }
-        </style>
     </head>
     <body>
-        <h1>Aš keičiu savo dydį!</h1>
-        <p>Aš keičiu savo spalvą!</p>
+        <img src="dice/<?php print rand(1, 6) ;?>.png ">
     </body>
 </html>
