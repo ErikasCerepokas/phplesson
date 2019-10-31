decc04888a3ee1c293b0cd47c11d66c17d57a32d
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-10-31 13:02:32 +0200

    uzduotys#5

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <?php
-$bin_vol = 40;
-$bin_heap_vol = rand(1, 15);
-$trash_per_day = 15;
+$distance = rand(50, 250); //Km
+$consumption = 7.5;// l/100km
+$price_l = 1.3;// Eur/l
 
-$atsakymas = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
+$kuras = round($distance * ($consumption / 100), 2); //Sunaudotas kuras
+$bendra = round($kuras*$price_l, 2); //Sunaudoto kuro kaina
 
-$siuksliu_prog = "Šiuklšių prognozė";
-$siukslines_turis = "Turima šiukšlinė - $bin_vol litrų";
-$gerai_kol = "Žmona nieko nesako, kol kaupas neviršija $bin_heap_vol litrų";
-$isvada = "Išvada: Nieko nedarysiu $atsakymas dienų.";
+$skaiciuokle = 'Kelionės skaičiuoklė';
+$kelias = "Nuvažiuota distancija: $distance";
+$sunaudota_kuro = "Sunaudota $kuras l. kuro";
+$kaina = "Kaina: $bendra pinigų";
 ?>
 <html>
     <head>
-        <title>UZDUOTIS#4</title>
+        <title>UZDUOTIS#5</title>
     </head>
     <body>
-        <h1><?php print $siuksliu_prog; ?></h1>
-        <p><?php print $siukslines_turis; ?></p>
-        <p><?php print $gerai_kol; ?></p>
-        <h3><?php print $isvada; ?></h3>
+        <h1><?php print $skaiciuokle; ?></h1>
+            <ul>
+                <li><?php print $kelias; ?></li>
+                <li><?php print $sunaudota_kuro; ?></li>
+                <li><?php print $kaina ?></li>
+            </ul>      
     </body>
 </html>
