fc0c7a68e298f33725fa90e0c8bf05ec55d6e53e
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-10-31 12:21:24 +0200

    UZDUOTYS#4

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
-
 $bin_vol = 40;
 $bin_heap_vol = rand(1, 15);
 $trash_per_day = 15;
 
-$atsakymas = floor(($bin_vol + $bin_heap_vol) / $trash_per_day );
+$atsakymas = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
 
 $siuksliu_prog = "Šiuklšių prognozė";
 $siukslines_turis = "Turima šiukšlinė - $bin_vol litrų";
 $gerai_kol = "Žmona nieko nesako, kol kaupas neviršija $bin_heap_vol litrų";
 $isvada = "Išvada: Nieko nedarysiu $atsakymas dienų.";
-        
-
 ?>
 <html>
     <head>
