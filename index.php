8808d783d0913914532dc0df0b6be7f54a290df2
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-10-31 11:28:34 +0200

    UZDUOTIS#3

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php
-$img_src = 'https://hips.hearstapps.com/esquireuk.cdnds.net/15/37/2048x2730/2048x2730-dumb-and-dumber-to-43-jpg-25867e58.jpg?resize=480:*'
+$skola = (rand(1, 100));
+$graza1 = (rand(101, 200));
+$graza2 = (rand(201, 300));
+
+$h1 = 'Skolos skaičiuoklė';
+$h2 = "Jei paėmei $skola jievrų";
+$h3 = "Su dviem kabančiais grąžinsi $graza2";
+$h4 = "Su vienu kabančiu grąžinsi $graza1";
 ?>
 <html>
     <head>
-        <title>IMG</title>
+        <title>UZDUOTIS#3</title>
     </head>
     <body>
-        <img src="<?php print $img_src ?>"/>
-        <img src="<?php print $img_src ?>"/>
-        <img src="<?php print $img_src ?>"/>
-        <img src="<?php print $img_src ?>"/>
+        <div>
+            <h1><?php print $h1; ?></h1>
+            <h2><?php print $h2; ?></h2>
+            <h3><?php print $h3; ?></h3>
+            <h3><?php print $h4; ?></h3>
+        </div>
     </body>
 </html>
