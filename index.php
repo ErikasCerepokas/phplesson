963ecdce82d3e8de3814d8d22c7a45af7fcc4a5a
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-11-04 11:13:20 +0200

    UZDUOTYS#4

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
-$distance = rand(50, 250); //Km
-$consumption = 7.5;// l/100km
-$price_l = 1.3;// Eur/l
+$grizai_velai = rand(0, 1);
+$grizai_isgeres = rand(0, 1);
+$h3_result = 'Nemiegosi';
 
-$kuras = round($distance * ($consumption / 100), 2); //Sunaudotas kuras
-$bendra = round($kuras*$price_l, 2); //Sunaudoto kuro kaina
+if ($grizai_velai && !$grizai_isgeres) {
+    $h2_result = 'Grįžai vėlai';
+    $h3_result = 'Miegosi';
+} else if ($grizai_velai && $grizai_isgeres) {
+    $h2_result = 'Grįžai vėlai ir išgėręs';
+} else if (!$grizai_velai && $grizai_isgeres) {
+    $h2_result = 'Grįžai išgėręs';
+} else { 
+    $h2_result = 'Nieko nepadarei';
+}
 
-$skaiciuokle = 'Kelionės skaičiuoklė';
-$kelias = "Nuvažiuota distancija: $distance";
-$sunaudota_kuro = "Sunaudota $kuras l. kuro";
-$kaina = "Kaina: $bendra pinigų";
+$h1 = 'Buitine skaiciuokle';
+$h2 = "Situacija: $h2_result";
+$h3 = "Isvada: $h3_result ant sofos"
 ?>
 <html>
     <head>
-        <title>UZDUOTIS#5</title>
+        <title>Buitine skaiciuokle</title>
     </head>
-    <body>
-        <h1><?php print $skaiciuokle; ?></h1>
-            <ul>
-                <li><?php print $kelias; ?></li>
-                <li><?php print $sunaudota_kuro; ?></li>
-                <li><?php print $kaina ?></li>
-            </ul>      
+    <body>  
+        <h1><?php print $h1; ?></h1>
+        <h2><?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
     </body>
 </html>
