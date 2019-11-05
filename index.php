f45a2bc1ff2dc591a19b0ce2b2aad26d88793aba
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-11-05 11:28:14 +0200

    uzduotys2

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
-<?php 
-$x = rand(1, 10);
+<?php
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$katasuniai = 0;
 
-for ($z = 1; $z < $x; $z++) { 
-    var_dump("Tai yra $z-tasis ciklas");
+for ($x = 0; $x < $kates; $x++) {
+    for ($z = 0; $z < $sunys; $z++) {
+        $pavyko = rand(0, 1);
+        if ($pavyko) {
+            $katasuniai++;
+            break;
+        }
+    }
 }
+$h1 = 'Katasuniu iseiga';
+$h2 = "Dalyvavo $kates kates ir $sunys sunys";
+$h3 = "Katasuniu iseiga: $katasuniai";
+?>
+<html>
+    <head>
+        <title>Uzduotis2</title> 
+    </head>
+    <body>
+        <main>
+            <h1><?php print $h1; ?></h1>
+            <h2><?php print $h2; ?></h2>
+            <h3><?php print $h3; ?></h3>
+        </main>
+    </body>
+</html>
