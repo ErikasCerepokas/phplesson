a6424696b951f6a5ed970deb993449873780ec7e
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-11-05 10:23:49 +0200

    uzduotys1

--- index.php
+++ index.php
@@ -1,41 +1,6 @@
 <?php 
-$bool = true;
-$int = 1 ;
-$flt = 1.23;
-$str = "1.23";
+$x = rand(1, 10);
 
-if ($bool == $int){
-    $type = 'Lygūs';
-} else {
-    $type = 'Identiški';
-    }
-if ($str == $bool) {
-    $type_1 = 'Lygūs';
-} else {
-    $type_1 = 'Identiški';
+for ($z = 1; $z < $x; $z++) { 
+    var_dump("Tai yra $z-tasis ciklas");
 }
-if ($flt == $str){
-    $type_2 = 'Lygūs';
-} else {
-    $type_2 = 'Nelygūs';
-}
-
-
-$li_1 = "Bool (true) ir Integer (1):$type";
-$li_2 = "String (1) ir Boolean (1):$type_1";
-$li_3 = "Float (1.23) ir String (1.23):$type_2"
-?>
-<html>
-    <head>
-        <title>Uzduotis#7</title>
-    </head>
-    <body>
-        <main>
-            <ul>
-                <li><?php print $li_1; ?></li>
-                <li><?php print $li_2; ?></li>
-                <li><?php print $li_3; ?></li>
-            </ul>
-        </main>
-    </body>
-</html>
