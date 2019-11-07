be62786c3fe189a7c83e5c315c47b47becb1f053
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-11-07 10:25:12 +0200

    array

--- index.php
+++ index.php
@@ -1,38 +1,14 @@
 <?php
-$days = 365;
-$pack_price = 3.50;
-$pack = 20;
-$data = date('N', strtotime('now'));
-$count_ttl = 0;
-
-for ($x = 1; $x < $days; $x++) {
-    $data = date('N', strtotime("+ $x days"));
-    if ($data < 5) {
-        $cigs_mon_fri = rand(3, 20);
-        $count_ttl += $cigs_mon_fri;
-    } else if ($data === 6) {
-        $cigs_sat = rand(15, 30);
-        $count_ttl += $cigs_sat;
-    } else if ($data === 7) {
-        $cigs_sun = rand(1, 5);
-        $count_ttl = $cigs_sun;
-    }
-}
-//Skaičiavimas cigarėčių
-$count = $count_ttl / $pack;
-$price_ttl = ceil($count * $pack_price);
-//Tekstas
-$h1 = 'Mano dūmų skaičiuoklė';
-$h2 = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eur.";
+$array = ['Kiausinis', 'Baklazanas', 'Obuolys'];
+var_dump($array);
 ?>
 <html>
     <head>
-        <title>Uzduotis4</title> 
+        
     </head>
     <body>
         <main>
-            <h1><?php print $h1 ?></h1>
-            <h2><?php print $h2 ?></h2>
-        </main>
+            
+        </main> 
     </body>
 </html>
