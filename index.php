eee20c924d3b7e051ca4aa626685aa075fed7d89
ErikasCerepokas <erikascerepokas@yahoo.com>
2019-11-07 11:24:34 +0200

    arejus

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <?php
-$array = ['Du dideli kiausiniai', 'vienas mazas baklazanas', 'Vienas didelis grietines indelis'];
-var_dump($array);
+$zmogaus_daiktas = [
+    [
+        'Vardas' => 'Petras',
+        'Pavardė' => 'Pizdzys',
+        'daiktai' => [
+                'pavadinimas' => 'telefonas',
+                'bukle' => 'prapistas',
+            ],                      
+    ],
+    [
+        'Vardas' => 'Tomas',
+        'Pavarde' => 'Ablomas',
+        'daiktai' =>
+        [
+            'pavadinimas' => 'buljonas',
+            'bukle' => 'naudojamas'
+        ], 
+    ],
+    [
+        'Vardas' => 'Anas',
+        'Pavarde' => 'Shitkovas',
+        'daiktai' =>
+        [
+            'pavadinimas' => 'rulonas',
+            'bukle' => 'rulonas',
+        ],
+    ],
+];
+var_dump($zmogaus_daiktas);
 ?>
 <html>
     <head>
